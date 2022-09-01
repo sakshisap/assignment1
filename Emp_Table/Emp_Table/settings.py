f6b9eee96f31cd19ce5a6e4048e8ff4f83415ae8
sakshi sapkale <sakshi.sapkale@t-systems.com>
2022-09-01 16:43:29 +0530

    git commit after heroku cli ins

--- Emp_Table/Emp_Table/settings.py
+++ Emp_Table/Emp_Table/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -120,7 +122,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATIC_ROOT =os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Activate Django-Heroku
+django_heroku.settings(locals())
+
+
