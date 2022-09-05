f859e8d0785ed1f29e8bcb89896d049e702a120f
sakshi sapkale <sakshi.sapkale@t-systems.com>
2022-09-05 11:16:50 +0530

    django-heroku deleted

--- Emp_Table/Emp_Table/settings.py
+++ Emp_Table/Emp_Table/settings.py
@@ -14,7 +14,7 @@ from pathlib import Path
 
 # changes for deploying
 import os
-import django_heroku
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -131,4 +131,3 @@ STATIC_ROOT= os.path.join(BASE_DIR,'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-django_heroku.settings(locals())
